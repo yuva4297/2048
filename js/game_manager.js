7c955b700951536769a3a75b5e0356a28ad24f85
yuva4297 <yuvaammu3@gmail.com>
2018-05-27 22:21:49 +0530

    Images are added instead of numbers

--- js/game_manager.js
+++ js/game_manager.js
@@ -167,7 +167,7 @@ GameManager.prototype.move = function (direction) {
           self.score += merged.value;
 
           // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          if (merged.value === 64) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
@@ -178,7 +178,6 @@ GameManager.prototype.move = function (direction) {
       }
     });
   });
-
   if (moved) {
     this.addRandomTile();
 
